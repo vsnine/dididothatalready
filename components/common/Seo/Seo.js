e7875cf3020a19040eaae8007f64bee41bc2e876
Josh Becigneul <josh@vsnine.org>
2021-05-18 10:17:57 -0400

    more tweaks

--- components/common/Seo/Seo.js
+++ components/common/Seo/Seo.js
@@ -15,14 +15,7 @@ export function SEO({ title, description = "" }) {
       <meta name="description" content={metaDescription} />
       <meta property="og:type" content="website" />
       <meta name="og:title" property="og:title" content={title} />
-      <meta
-        name="og:description"
-        property="og:description"
-        content={metaDescription}
-      />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={metaDescription} />
+      <meta name="og:description" property="og:description" content={metaDescription} />
       <link rel="icon" type="image/png" href="/static/favicon.ico" />
       <link rel="apple-touch-icon" href="/static/favicon.ico" />
       <link rel="stylesheet" href="8ballstyle.css" />
